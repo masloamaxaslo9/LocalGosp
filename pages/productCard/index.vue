1a83011e52572beec313d605a391b36a4ad6cc1f
Iryna-L <iryna.lukashuk.97@gmail.com>
2019-05-27 16:20:26 +0300

    logo

---
+++ pages/productCard/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="card">
+    <h1 class="card_title">title</h1>
+    <img class="card_img" src="" alt="">
+    <p class="card_description">description</p>
+    <p class="card_price">price</p>
+    <p class="card_category">category</p>
+    <button class="card_btn">в корзинку</button>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
