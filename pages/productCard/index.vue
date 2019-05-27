5bf2f30d5ad14e88cb141bd79b013eade9aebb3b
Iryna-L <iryna.lukashuk.97@gmail.com>
2019-05-27 16:23:28 +0300

    logo

--- pages/productCard/index.vue
+++ pages/productCard/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="card">
+    <left-sidebar></left-sidebar>
     <h1 class="card_title">title</h1>
     <img class="card_img" src="" alt="">
     <p class="card_description">description</p>
@@ -10,8 +11,11 @@
 </template>
 
 <script>
+import leftSidebar from "../../components/LeftSidebar.vue"
 export default {
-
+  components: {
+    leftSidebar
+  }
 }
 </script>
 
